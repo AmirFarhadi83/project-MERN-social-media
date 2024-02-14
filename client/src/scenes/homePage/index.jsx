36df956493a60f19f1b92de3cbfa09b5cc56287f
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-02-14 20:03:45 +0330

    first push

---
+++ client/src/scenes/homePage/index.jsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+import Navbar from "scenes/navbar";
+import React from 'react';
+
+export default function HomePage() {
+    return (
+        <>
+            <Box>
+                <Navbar />
+            </Box>
+        </>
+    )
+}
