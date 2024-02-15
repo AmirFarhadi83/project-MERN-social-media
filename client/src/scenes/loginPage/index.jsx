15fce1e86052df1572b7e3e75bed232304994d3c
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-02-15 15:53:18 +0330

    update

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Box, Typography, useTheme, useMediaQuery } from '@mui/material';
-
+import Form from "./Form";
 
 export default function LoginPage() {
   const theme = useTheme();
@@ -9,13 +9,25 @@ export default function LoginPage() {
   return (
     <>
       <Box>
-        <Box>
-          <Typography
-            fontWeight={"bold"}
-            fontSize={"32px"}
-            color={"primary"}
-          >
+        <Box
+          width={"100%"}
+          backgroundColor={theme.palette.background.alt}
+          p={"1rem 6%"}
+          textAlign={"center"}
+        >
+          <Typography fontWeight={"bold"} fontSize={"32px"} color={"primary"}>
             شبکه اجتماعی
+          </Typography>
+        </Box>
+
+        <Box width={isNonMobileScreens ? "50%" : "93%"}
+        p={"2rem"}
+        m={"2rem auto"}
+        borderRadius={"1.5rem"}
+        backgroundColor={theme.palette.background.alt}
+        >
+          <Typography fontWeight={"500"} variant='h5' sx={{mb:"1.5rem"}}>
+          به شبکه اجتماعی خوش آمدید، شبکه اجتماعی برای گریزان از اجتماع!!
           </Typography>
         </Box>
       </Box>
