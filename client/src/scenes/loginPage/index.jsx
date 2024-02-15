f2286a82dd9e475d9512f0e5bca03190fd79e174
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-02-15 12:46:58 +0330

    Update index.jsx

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Box, Typography, useTheme, useMediaQuery } from '@mui/material';
+
 
 export default function LoginPage() {
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width:1010px)");
+
   return (
-    <div>LoginPage</div>
+    <>
+      <Box>
+        <Box>
+          <Typography
+            fontWeight={"bold"}
+            fontSize={"32px"}
+            color={"primary"}
+          >
+            شبکه اجتماعی
+          </Typography>
+        </Box>
+      </Box>
+    </>
   )
 }
