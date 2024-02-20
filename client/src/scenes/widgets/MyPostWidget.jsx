fdf2b73f14e8abe8c742a7409f4123d094a5c017
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-02-20 14:27:58 +0330

    Update MyPostWidget.jsx

--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -1,5 +1,5 @@
- import React from 'react';
- import {
+import React from 'react';
+import {
     EditOutlined,
     DeleteOutlined,
     AttachFileOutlined,
@@ -7,8 +7,8 @@
     ImageOutlined,
     MicOutlined,
     MoreHorizOutlined,
-  } from "@mui/icons-material";
-  import {
+} from "@mui/icons-material";
+import {
     Box,
     Divider,
     Typography,
@@ -17,21 +17,72 @@
     Button,
     IconButton,
     useMediaQuery,
-  } from "@mui/material";
-  import FlexBetween from "components/FlexBetween";
-  import Dropzone from "react-dropzone";
-  import UserImage from "components/UserImage";
-  import WidgetWrapper from "components/WidgetWrapper";
-  import { useState } from "react";
-  import { useDispatch, useSelector } from "react-redux";
-  import { setPosts } from "state";
- 
-  
-  export default function MyPostWidget() {
+} from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import Dropzone from "react-dropzone";
+import UserImage from "components/UserImage";
+import WidgetWrapper from "components/WidgetWrapper";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPosts } from "state";
+
+
+export default function MyPostWidget({ picturePath }) {
+    const dispatch = useDispatch();
+    const [isImage, setIsImage] = useState(false);
+    const [image, setImage] = useState(null);
+    const [post, setPost] = useState("");
+    const { palette } = useTheme();
+    const { _id } = useSelector((state) => state.user);
+    const token = useSelector((state) => state.token);
+    const isNonMobileScreens = useMediaQuery('(min-width:1010px)');
+    const mediumMain = palette.neutral.mediumMain;
+    const medium = palette.neutral.medium;
+
+    const handlePost = async () => {
+        const formData = new FormData();
+        formData.append("userId", _id);
+        formData.append("description", post)
+        if (image) {
+            formData.append("picture", image);
+            formData.append("picturePath", image.name);
+        }
+        const response = await fetch(`http://localhost:3001/posts`, {
+            method: "POST",
+            headers: { Authorization: `Bearer ${token}` },
+            body: formData,
+        });
+        const posts = await response.json();
+        dispatch(setPosts({ posts }));
+        setImage(null);
+        setPost("");
+    };
+
     return (
-      <>
-      
-      </>
+        <>
+    <WidgetWrapper>
+        <FlexBetween gap={"1.5rem"}>
+            <UserImage image={picturePath} />
+            <InputBase
+            placeholder='توی فکرت چی میگذره...؟'
+            onChange={(e)=>setPost(e.target.value)}
+            value={post}
+            sx={{
+                width:"100%",
+                backgroundColor:palette.neutral.light,
+                borderRadius:"2rem",
+                padding:"1rem 2rem",
+            }}
+            />
+        </FlexBetween>
+        {isImage &&(
+            <Box
+            
+            >
+
+            </Box>
+        )}
+    </WidgetWrapper>
+        </>
     );
-  };
-  
+};
