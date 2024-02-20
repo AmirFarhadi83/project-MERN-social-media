e6434e11a12624e508899e0c9648f66b8db55dc8
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-02-20 20:27:34 +0330

    Update Friend.jsx

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -22,9 +22,28 @@ export default function Friend() {
 
     const isFriend = friends.find((friend) => friend._id === friendId)
 
+    const patchFriend = async () => {
+        const response = await fetch(
+            `http://localhost:3001/users/${_id}/${friendId}`,
+            {
+                method: "PATCH",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json",
+                },
+            }
+        );
+        const data = await response.json();
+        dispatch(setFriends({ friends: data }));
+    };
+
     return (
         <>
-
+            <FlexBetween>
+                <FlexBetween gap={"1rem"}>
+                    <UserImage image={userPicturePath} size='55px' />
+                </FlexBetween>
+            </FlexBetween>
         </>
     );
 };
