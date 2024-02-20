4d76a7549fd723c736ed2de7ffe5edbc24bea6c6
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-02-20 23:20:05 +0330

    Update index.jsx

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -5,6 +5,7 @@ import { useSelector } from "react-redux";
 import UserWidget from "scenes/widgets/UserWidget";
 import MyPostWidget from "scenes/widgets/MyPostWidget";
 import PostsWidget from "scenes/widgets/PostsWidget";
+import AdvertWidget from "scenes/widgets/AdvertWidget";
 
 export default function HomePage() {
     const isNonMobileScreens = useMediaQuery("(min-width:1010px)");
@@ -33,7 +34,10 @@ export default function HomePage() {
                         <MyPostWidget picturePath={picturePath} />
                         <PostsWidget userId={_id} />
                     </Box>
-                    {isNonMobileScreens && <Box flexBasis={"26%"}></Box>}
+                    {isNonMobileScreens && <Box flexBasis={"26%"}>
+                        <AdvertWidget />
+                        <Box m={"2rem 0"} />
+                    </Box>}
                 </Box>
             </Box>
         </>
