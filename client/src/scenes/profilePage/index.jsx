8173f5f00ab64f890a530c14cf9714c8ac4cb735
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-02-20 23:46:03 +0330

    Update index.jsx

--- client/src/scenes/profilePage/index.jsx
+++ client/src/scenes/profilePage/index.jsx
@@ -13,7 +13,7 @@ export default function ProfilePage() {
   const [user, setUser] = useState(null);
   const { userId } = useParams();
   const token = useSelector((state) => state.token);
-  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  const isNonMobileScreens = useMediaQuery("(min-width:1010px)");
 
   const getUser = async () => {
     const response = await fetch(`http://localhost:3001/users/${userId}`, {
