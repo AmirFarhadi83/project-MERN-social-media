04314be6dcdcfbe90db4daf64b1c1d666ef76adc
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-02-20 23:00:10 +0330

    Update PostWidget.jsx

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -74,6 +74,7 @@ export default function PostWidget({
         )}
         <FlexBetween mt={"0.25rem"}>
           <FlexBetween gap={"1rem"}>
+
             <FlexBetween gap={"0.3rem"}>
               <IconButton
                 onClick={() => patchLike()}
@@ -86,15 +87,35 @@ export default function PostWidget({
               </IconButton>
               <Typography>{likeCount}</Typography>
             </FlexBetween>
+
             <FlexBetween gap={"0.3rem"}>
               <IconButton
                 onClick={() => setIsComments(!isComments)}
               >
                 <ChatBubbleOutlineOutlined />
               </IconButton>
+              <Typography>{comments.length}</Typography>
             </FlexBetween>
+
           </FlexBetween>
+
+          <IconButton>
+            <ShareOutlined />
+          </IconButton>
         </FlexBetween>
+        {isComments && (
+          <Box mt="0.5rem">
+            {comments.map((comment, i) => (
+              <Box key={`${name}-${i}`}>
+                <Divider />
+                <Typography sx={{ color: main, m: "0.5rem 0", pl: "1rem" }}>
+                  {comment}
+                </Typography>
+              </Box>
+            ))}
+            <Divider />
+          </Box>
+        )}
       </WidgetWrapper>
     </>
   )
