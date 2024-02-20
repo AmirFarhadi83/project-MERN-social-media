64b3b858b844798dee9fc7ab990443be1b7e6b73
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-02-20 23:44:39 +0330

    Update index.jsx

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -6,6 +6,7 @@ import UserWidget from "scenes/widgets/UserWidget";
 import MyPostWidget from "scenes/widgets/MyPostWidget";
 import PostsWidget from "scenes/widgets/PostsWidget";
 import AdvertWidget from "scenes/widgets/AdvertWidget";
+import FriendListWidget from "scenes/widgets/FriendListWidget";
 
 export default function HomePage() {
     const isNonMobileScreens = useMediaQuery("(min-width:1010px)");
@@ -37,6 +38,7 @@ export default function HomePage() {
                     {isNonMobileScreens && <Box flexBasis={"26%"}>
                         <AdvertWidget />
                         <Box m={"2rem 0"} />
+                        <FriendListWidget userId={_id} />
                     </Box>}
                 </Box>
             </Box>
