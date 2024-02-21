24eddbf48b3f1baab574f51c449cb474928f0ca1
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-02-21 20:18:51 +0330

    Update PostsWidget.jsx

--- client/src/scenes/widgets/PostsWidget.jsx
+++ client/src/scenes/widgets/PostsWidget.jsx
@@ -10,22 +10,25 @@ export default function PostsWidget({ userId, isProfile = false }) {
     const token = useSelector((state) => state.token);
 
     const getPosts = async () => {
-        const response = await fetch(`http://localhost:3001/posts`, {
+        const response = await fetch("http://localhost:3001/posts", {
             method: "GET",
             headers: { Authorization: `Bearer ${token}` },
         });
         const data = await response.json();
         dispatch(setPosts({ posts: data }));
-    }
+    };
 
     const getUserPosts = async () => {
-        const response = await fetch(`http://localhost:3001/posts/${userId}/posts`, {
-            method: "GET",
-            headers: { Authorization: `Bearer ${token}` },
-        });
+        const response = await fetch(
+            `http://localhost:3001/posts/${userId}/posts`,
+            {
+                method: "GET",
+                headers: { Authorization: `Bearer ${token}` },
+            }
+        );
         const data = await response.json();
         dispatch(setPosts({ posts: data }));
-    }
+    };
 
     useEffect(() => {
         if (isProfile) {
@@ -33,39 +36,42 @@ export default function PostsWidget({ userId, isProfile = false }) {
         } else {
             getPosts();
         }
-    }, []); //eslint-disable-line react-hooks/exhaustive-deps
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-    if(!posts) return null;
+    if (!posts) return null;
     // const postList=()=>{
     //     return posts.map((post)=> <PostWidget key={post._id} post={post}/>)
     // };
+    console.log(posts)
     return (
         <>
-            {posts?.map(({
-                _id,
-                userId,
-                firstName,
-                lastName,
-                description,
-                location,
-                picturePath,
-                userPicturePath,
-                likes,
-                comments,
-            }) => (
-                <PostWidget
-                    key={_id}
-                    postId={_id}
-                    postUserId={userId}
-                    name={`${firstName} ${lastName}`}
-                    description={description}
-                    location={location}
-                    picturePath={picturePath}
-                    userPicturePath={userPicturePath}
-                    likes={likes}
-                    comments={comments}
-                />
-            ))}
+            {posts.map(
+                ({
+                    _id,
+                    userId,
+                    firstName,
+                    lastName,
+                    description,
+                    location,
+                    picturePath,
+                    userPicturePath,
+                    likes,
+                    comments,
+                }) => (
+                    <PostWidget
+                        key={_id}
+                        postId={_id}
+                        postUserId={userId}
+                        name={`${firstName} ${lastName}`}
+                        description={description}
+                        location={location}
+                        picturePath={picturePath}
+                        userPicturePath={userPicturePath}
+                        likes={likes}
+                        comments={comments}
+                    />
+                )
+            )}
         </>
     );
 };
